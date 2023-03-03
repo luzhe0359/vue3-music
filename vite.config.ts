380ec981c9830d17ec2ce048fe17df044138e212
luzhe0359 <luzhe0359@163.com>
2023-03-03 22:12:33 +0800

    ci: 配置静态站点

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue3-music/',
   plugins: [
     vue(),
     AutoImport({
