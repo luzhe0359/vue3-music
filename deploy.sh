9d1de051b2a43ae8bb7a0fc01201e32996f7e631
luzhe0359 <luzhe0359@163.com>
2023-03-04 10:32:33 +0800

    feature: deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+# 发生任何错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 进入输出产物文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:luzhe0359/vue3-music.git main:gh-pages
+
+cd -
